6d3c7b9aca8bc893f37229a8f88d1e092d148222
Kashyrets Ivanna <ivanna.kashyrets@ex2.team>
2023-08-22 16:51:06 +0300

    feat: test, lint, prettier, husky commit hook

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return ({ message: 'Hello API' });
+    console.log('f');
+    return { message: 'Hello API' };
   }
 }
